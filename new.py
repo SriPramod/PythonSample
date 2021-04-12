fa8313fb5136b3cee7f7a2922dbed188864f3db6
“SriPramod” <bhpramod62@gmail.com>
2021-04-12 19:12:47 +0530

    first change

--- new.py
+++ new.py
@@ -2,7 +2,6 @@ print('Hello Welcome to Github')
 ######## New ######
 print('first change from local Pc')
 print('First change from Github')
-
 print('second change from github')
 print('second change from local')
 
